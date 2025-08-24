040e968ac1bea1448c6c7e0dbbf3216b435d1f26
Chiken1024 <martinmarinauk@gmail.com>
2025-08-24 18:10:34 +0200

    Add files via upload

--- l5.py
+++ l5.py
@@ -4,33 +4,35 @@ from PIL import Image
 from PIL.ImageFile import ImageFile
 
 
+IMAGES: list[str] = os.listdir("./L5/Images")
+edited_images: list[str] = os.listdir("./L5/Edited Images")
+
+
 def generate_video(avg_width: int, avg_height: int) -> cv.VideoCapture:
   video: cv.VideoWriter = cv.VideoWriter(
     "./L5/video.avi", 0, 1, (avg_width, avg_height)
   )
   for image in edited_images:
     video.write(cv.imread(f"./L5/Edited Images/{image}"))
-  cv.destroyAllWindows()
-  video.release()
+  return video
 
 
-images: list[str] = os.listdir("./L5/Images")
-edited_images: list[str] = os.listdir("./L5/Edited Images")
-
-
-if len(edited_images) < len(images):
+if len(edited_images) != len(IMAGES):
+  for image in edited_images:
+    os.remove(os.path.join("./L5/Edited Images", image))
+  
   IMAGES_PATH: str = "./L5/Images"
   
   avg_width, avg_height = 0, 0
 
-  for path in images:
+  for path in IMAGES:
     image: ImageFile = Image.open(os.path.join(IMAGES_PATH, path))
     avg_width += image.width
     avg_height += image.height
 
-  avg_width, avg_height = avg_width // len(images), avg_height // len(images)
+  avg_width, avg_height = avg_width // len(IMAGES), avg_height // len(IMAGES)
 
-  for path in images:
+  for path in IMAGES:
     if path.endswith(".jpg") or path.endswith(".png"):
       image = Image.open(os.path.join(IMAGES_PATH, path)).resize(
         (avg_width, avg_height)
@@ -44,4 +46,4 @@ else:
   ).shape[:2][::-1]
 
 
-generate_video(avg_width, avg_height)
+generate_video(avg_width, avg_height).release()
