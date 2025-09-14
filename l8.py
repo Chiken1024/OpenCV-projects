55f697fb225c413066c14f43e5224a298d2bb838
Chiken1024 <martinmarinauk@gmail.com>
2025-09-14 18:22:18 +0200

    Add files via upload

--- l8.py
+++ l8.py
@@ -1,17 +1,19 @@
 import cv2 as cv
-import os
 
 
 PATH: str = "L8/"
 
-VIDEO_SOURCE: str = PATH + "cars.mp4"
+VIDEO_SOURCE: str = PATH + "cars.avi"
 CASCADE_PATH: str = PATH + "cars.xml"
+
+FPS: int = 30
+SCALE_FACTOR: float = 1.5
 
 
 try:
   open(VIDEO_SOURCE)
 except:
-  raise FileNotFoundError("Failed to load video")
+  raise FileNotFoundError("Failed to locate video file")
   exit()
 else:
   capture: cv.VideoCapture = cv.VideoCapture(VIDEO_SOURCE)
@@ -19,13 +21,16 @@ else:
 try:
   open(CASCADE_PATH)
 except:
-  raise FileNotFoundError("Failed to load cascade")
+  raise FileNotFoundError("Failed to locate cascade file")
   exit()
 else:
   car_cascade: cv.CascadeClassifier = cv.CascadeClassifier(CASCADE_PATH)
   if car_cascade.empty():
     raise ValueError("Failed to load cascade classifier")
     exit()
+
+
+WAIT_DURATION: int = 1000 // FPS
 
 
 while True:
@@ -41,16 +46,23 @@ while True:
   )
 
   for (x, y, w, h) in cars:
-    cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
-  
-  cv.putText(
-    frame, f"Cars detected: {len(cars)}", (10, 30),
-    cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2
-  )
+    cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+
+  if SCALE_FACTOR != 1.0:
+    frame: cv.Mat = cv.resize(
+      frame, [int(i * SCALE_FACTOR) for i in frame.shape[:2][::-1]], frame
+    )
+
+  cars_amount: int = len(cars)
+  [
+    cv.putText(
+      frame, f"Cars detected: {cars_amount}", (10, 40 - i * 3),
+      cv.FONT_HERSHEY_SIMPLEX, 1, (i * 255, i * 255, i * 255), 2
+    ) for i in range(2)
+  ]
 
   cv.imshow("Car Detection", frame)
-
-  if cv.waitKey(33) == 27: break
+  if cv.waitKey(WAIT_DURATION) == 27: break
 
 
 capture.release()
